3d08a8ce297f210e6f8e8ff3b85a226fd0d85245
Yusuke Ise <129273697+ise-bachelor@users.noreply.github.com>
2024-12-11 14:07:30 +0900

    thumnailsファイルを作成動作はまだ

---
+++ src/pages/thumbnails.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useRef } from "react";
+
+export default function Thumbnails({ img, setImg }: { img: string[], setImg: React.Dispatch<React.SetStateAction<string[]>> }) {
+
+  return(
+  <>
+  
+  </>)
+
+}
