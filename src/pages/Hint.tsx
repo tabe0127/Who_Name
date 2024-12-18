ff7b506108c05fb0d1bd8f2fe1f79778d224c39b
Yusuke Ise <129273697+ise-bachelor@users.noreply.github.com>
2024-12-18 14:09:47 +0900

    画面遷移をファイルごとに切り分けとStart画面を作成

--- src/pages/Hint.tsx
+++ src/pages/Hint.tsx
@@ -1,40 +1,40 @@
-import React, { useEffect, useRef } from "react";
+// import React, { useEffect, useRef } from "react";
 
-export default function Hint({ isOpenHint, setIsOpenHint }: { isOpenHint: boolean, setIsOpenHint: React.Dispatch<React.SetStateAction<boolean>> }) {
+export default function Hint() {
 
-    // ---------------------------------------------
-    // モーダル外をクリックした時の処理
-    // ---------------------------------------------
-    const HintRef = useRef(null);
-    useEffect(() => {
-        function handleClickOutside(event: MouseEvent) {
-            if (HintRef.current && !(HintRef.current as HTMLElement).contains(event.target as Node)) {
-                setIsOpenHint(false);
-            }
-        }
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, [HintRef, setIsOpenHint]);
+    // // ---------------------------------------------
+    // // モーダル外をクリックした時の処理
+    // // ---------------------------------------------
+    // const HintRef = useRef(null);
+    // useEffect(() => {
+    //     function handleClickOutside(event: MouseEvent) {
+    //         if (HintRef.current && !(HintRef.current as HTMLElement).contains(event.target as Node)) {
+    //             setIsOpenHint(false);
+    //         }
+    //     }
+    //     document.addEventListener("mousedown", handleClickOutside);
+    //     return () => {
+    //         document.removeEventListener("mousedown", handleClickOutside);
+    //     };
+    // }, [HintRef, setIsOpenHint]);
 
 
-    // ---------------------------------------------
-    // モーダル表示中: 背面のスクロールを禁止
-    // ---------------------------------------------
-    useEffect(() => {
-        if (isOpenHint) {
-            document.body.classList.add('overflow-hidden');
-        } else {
-            document.body.classList.remove('overflow-hidden');
-        }
-    }, [isOpenHint]);
+    // // ---------------------------------------------
+    // // モーダル表示中: 背面のスクロールを禁止
+    // // ---------------------------------------------
+    // useEffect(() => {
+    //     if (isOpenHint) {
+    //         document.body.classList.add('overflow-hidden');
+    //     } else {
+    //         document.body.classList.remove('overflow-hidden');
+    //     }
+    // }, [isOpenHint]);
 
     return (
         <>
-            {isOpenHint &&
+            {/* {isOpenHint && */}
                 <div className="fixed z-10 top-0 left-0 w-full h-full bg-black bg-opacity-50">
-                    <div className="relative z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-h-[95vh] md:max-h-[90vh] w-[97vw] md:w-[80vw] p-4 md:p-10 md:pb-20 bg-slate-100 border-2 border-neutral-950 shadow-lg rounded-xl overflow-auto text-black " ref={HintRef}>
+                    <div className="relative z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-h-[95vh] md:max-h-[90vh] w-[97vw] md:w-[80vw] p-4 md:p-10 md:pb-20 bg-slate-100 border-2 border-neutral-950 shadow-lg rounded-xl overflow-auto text-black ">
 
                         {/* ここにヒントの中身 */}
                         <ul>
@@ -48,7 +48,7 @@ export default function Hint({ isOpenHint, setIsOpenHint }: { isOpenHint: boolea
 
                     </div>
                 </div>
-            }
+            {/* } */}
         </>
 
     );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,182 +1,20 @@
-import { useState, useRef } from 'react'
+import { useState } from 'react'
 import React from 'react'
-import Modal from './Modal'
-import Hint from './Hint'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Start from './Start'
+import Hint from './Hint'
 
 export default function Home() {
-  // 顔写真と名前の管理
-  const [entries, setEntries] = useState<{ img: string; name: string }[]>([]);
-  const [count, setCount] = useState(0); // 現在表示中のエントリインデックス
-
-  // 登録フォームの状態管理
-  const [tempImg, setTempImg] = useState<string | null>(null);
-  const [tempName, setTempName] = useState('');
-
-  // モーダルの管理
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
-  // スタート画面の表示のON・OFF
-  const [showStartScreen, setShowStartScreen] = useState(true);
-
-  // スタート画面上のStartボタンを表示するタイミングを検討する時間
-  const [showStartScreen_StartButton, setShowStartScreen_StartButton] = useState(false);
-
-  // ゲーム画面の表示のON・OFF
-  const [showGameScreen, setShowGameScreen] = useState(false);
-
-  // 一時的な画像アップロード処理
-  const handleTempImgUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files) return;
-    const tempFile = e.target.files[0];
-    const tempImgUrl = window.URL.createObjectURL(tempFile);
-    setTempImg(tempImgUrl);
-  };
-
-  // エントリ登録処理
-  const handleRegisterEntry = () => {
-    if (!tempImg || tempName.trim() === '') {
-      alert('写真と名前の両方を入力してください！');
-      return;
-    }
-
-    setEntries([...entries, { img: tempImg, name: tempName }]);
-    setTempImg(null); // 一時的な画像をリセット
-    setTempName(''); // 名前入力をリセット
-    setShowStartScreen_StartButton(true);  // ゲームスタートボタンを表示
-  };
-
-  // ゲームスタート処理
-  const GameStart = () => {
-    setShowStartScreen(false);
-    setShowStartScreen_StartButton(false);
-    setShowGameScreen(true);
-  }
-
-  const filePickerRef = useRef<HTMLInputElement>(null);
-  const showFolder = () => {
-    if (filePickerRef.current) {
-      filePickerRef.current.click();
-    }
-  };
-
-  // 遊び方説明のモーダルを管理
-  const [isOpenHint, setIsOpenHint] = React.useState(false);
+  const [sceneController, setSceneController] = useState<string>('Start')
 
   return (
     <div className={styles.container}>
       <h1 className="text-5xl">Who?Name!</h1>
 
-      {/* --- ボタン --- */}
-      <button
-        onClick={() => setIsOpenModal(true)}
-        className="my-4 p-4 text-white font-bold bg-blue-400 rounded-xl shadow-lg"
-      >
-        遊び方
-      </button>
+      {sceneController=='Start' ? <Start setSceneController={setSceneController}></Start>: <></>}
+      {sceneController=='Name' ? <Hint></Hint>:<></>}
+      <p>{sceneController}</p>
 
-
-      {/* アプリ起動時（ゲーム開始前）に表示する画面 */}
-      {/* --- 一括登録フォーム --- */}
-      {showStartScreen && (
-        <div className="my-4 p-4 border rounded-lg shadow-md">
-          <h2 className="text-2xl mb-2">写真と名前を登録</h2>
-
-          {/* 名前入力 */}
-          <input
-            type="text"
-            value={tempName}
-            onChange={(e) => setTempName(e.target.value)}
-            placeholder="名前を入力"
-            className="mb-2 p-2 border rounded-md w-full"
-          />
-
-          {/* ファイルアップロード */}
-          <input
-            type="file"
-            accept="image/*"
-            ref={filePickerRef}
-            onChange={handleTempImgUpload}
-            className="mb-2"
-            hidden
-          />
-          <button type="button" onClick={showFolder} className="my-4 p-4 text-white font-bold bg-blue-400 rounded-xl shadow-lg mx-2">写真をアップロードする</button>
-
-          {/* プレビュー */}
-          {tempImg && (
-            <div className="mb-2">
-              <Image
-                src={tempImg}
-                alt="Preview"
-                width={150}
-                height={150}
-                className="rounded-lg"
-              />
-            </div>
-          )}
-
-          {/* 登録ボタン */}
-          <button
-            onClick={handleRegisterEntry}
-            className="p-2 bg-green-500 text-white rounded-md shadow-lg"
-          >
-            登録
-          </button>
-        </div>
-      )}
-
-      {/* スタート画面からゲーム画面に遷移するためのボタン */}
-      {showStartScreen_StartButton && (
-        <div>
-          {/* 登録ボタン */}
-          <button
-            onClick={GameStart}
-            className="p-2 bg-red-500 text-white rounded-md shadow-lg"
-          >
-            Game Start
-          </button>
-        </div>
-      )}
-
-      {/* ゲーム開始時に表示する画面 */}
-      {showGameScreen && (
-        <div>
-        {/* --- 表示 --- */}
-        {entries.length > 0 && (
-          <div className="p-4">
-            <Image
-              src={entries[count].img}
-              alt={`Sample ${count + 1}`}
-              width={300}
-              height={300}
-              layout='responsive'
-              className='max-h-72'
-            />
-            <p>{entries[count].name}</p>
-          </div>
-        )}
-
-        {/* --- Nextボタン --- */}
-        {entries.length > 0 && (
-          <div className={styles.card}>
-            <button
-              onClick={() => setCount(Math.floor(Math.random() * entries.length))}
-              className="p-4 text-white font-bold bg-blue-400 rounded-xl shadow-lg m-2"
-            >
-              Next
-            </button>
-            {/* --- ヒントボタン --- */}
-            <button onClick={()=>setIsOpenHint(true)} className="my-4 p-4 text-white font-bold bg-blue-400 rounded-xl shadow-lg">名付けのヒント</button>
-          </div>
-        )}
-        </div>
-      )}
-
-      {/* --- モーダル --- */}
-      <Modal isOpenModal={isOpenModal} setIsOpenModal={setIsOpenModal} />
-      {/* --- ヒント --- */}
-      <Hint isOpenHint={isOpenHint} setIsOpenHint={setIsOpenHint} />
     </div>
   );
 }
