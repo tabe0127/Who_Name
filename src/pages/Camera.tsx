023b71e7242f6e20168613c66b597e2e8fa03bdd
shosuke447 <shosuke447@gmail.com>
2025-01-16 17:14:58 +0900

    不要なものを消した

--- src/pages/Camera.tsx
+++ src/pages/Camera.tsx
@@ -1,9 +1,8 @@
 import React, { useRef, useCallback } from 'react';
 import Webcam from 'react-webcam';
-import Image from "next/image"
 
 
-const WebCamera = ({tempImg, setTempImg }: { tempImg: string, setTempImg : React.Dispatch<React.SetStateAction<string>>}) => {
+const WebCamera = ({ setTempImg }: { setTempImg : React.Dispatch<React.SetStateAction<string | null>>}) => {
   const webcamRef = useRef<Webcam>(null);
 
   const capture = useCallback(() => {
@@ -18,8 +17,7 @@ const WebCamera = ({tempImg, setTempImg }: { tempImg: string, setTempImg : React
         ref={webcamRef}
         screenshotFormat="image/jpeg"
       />
-      <button onClick={capture}>撮影</button>
-      {tempImg && <Image src={tempImg} alt="captured" width={150} height={150} />}
+      <button onClick={capture} className="my-4 p-4 text-white font-bold bg-blue-400 rounded-xl shadow-lg">撮影</button>
     </>
   );
 };
